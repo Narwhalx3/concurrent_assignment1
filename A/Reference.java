cfbbc71d198fde1a51673026f7b99b3021c0cac2
Narwhalx3 <36626217+Narwhalx3@users.noreply.github.com>
2018-02-19 14:01:09 +0100

    Update Reference.java

--- A/Reference.java
+++ A/Reference.java
@@ -4,15 +4,5 @@ package concurrent_assignment1.A;
  * Get the reference of the main thread */
 
 public class Reference {
-  int i=1, num=0;
-        while(i<=50){
-            if(num%2 != 0){
-                System.out.println("Odd numer " + i + ": " + num + "\n");
-                i++;
-                num++;
-            }
-            else{
-                num++;
-            }
-        }
+  
 }
