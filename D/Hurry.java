eef16da274ed55de7241790285471d8bba4b7da7
KateGJ <36626226+KateGJ@users.noreply.github.com>
2018-02-28 09:01:26 +0100

    Update Hurry.java

--- D/Hurry.java
+++ D/Hurry.java
@@ -1,17 +1,34 @@
-package concurrent_assignment1.D;
-/**
- * Hurry must:
- * - Be the main thread
- * - Create the Lazy thread.
- * - Wait for Lazy to finish. Each second it must print the message: “Aren’t you ready yet?” and checks if Lazy is finished.
- * - If Lazy isn’t finished after 5 seconds, Hurry will claim “You are resting in your laurels… and I am leaving!” and interrupts Lazy. Then it waits for Lazy to finish.
- * - However, if Lazy finishes before the 5 seconds deadline, Hurry says “At last, a turtle runs rings round you!”.
- * 
- * @author yournamehere
- *
- */
+
+package D;
 
 
 public class Hurry {
 
+    public static Boolean fin = false;
+    
+    public static void main(String[] args) throws InterruptedException {
+        int n=0;
+        Thread h = Thread.currentThread();
+        h.setName("Hurry: ");
+        System.out.println(h.getName()+"Aren't you ready yet?");
+        Thread Lazy;
+        
+        Lazy = new Thread(new Lazy()); //Creates the Lazy thread
+        Lazy.start();
+        
+        while(!fin){    //Waits to Lazy to finish. Each second shows the message and checks if it is finished
+            h.sleep(1000);
+            System.out.println(h.getName()+"Aren't you ready yet?");
+            n++;
+            if((n==5) && !fin){    //If Lazy no finish after 5 seconds Hurry will claim
+                System.out.println(h.getName()+"You are resting in your laurels... and I am leaving!"); 
+                Lazy.interrupt(); //and interrupts Lazy
+                fin = true;
+            }
+        }
+        Lazy.join();
+        if(n<5) 
+            System.out.println(h.getName()+"At last, a turtle runs rings round you!");
+    }
+    
 }
