a3fa2db9ec80ea1009413abac82b04837d9ba1b5
KateGJ <36626226+KateGJ@users.noreply.github.com>
2018-02-28 08:59:54 +0100

    Update T_PrintID.java

--- B/T_PrintID.java
+++ B/T_PrintID.java
@@ -1,13 +1,25 @@
-package concurrent_assignment1.B;
+package B;
 
-/**
- * Create class T_PrintID which extends Thread:
- *	- It must contain a variable ID whose value is first set through the class constructor.
- *	- Method run() has a loop which iterates 10 times displaying ID.
- *
- * @author yournamehere
- */
-
-public class T_PrintID {
-
+public class T_PrintID extends Thread{
+    
+    int ID, n;
+    
+    
+    //CONSTRUCTOR
+    T_PrintID(int ID, int n){
+        this.ID = ID;
+        this.n = n;
+    }
+    
+    
+    
+    //METHOD
+    public void run(){
+        for (int i = 0; i < n; i++) {
+            System.out.println("I'm process: " + ID);
+        }
+    }
+    
+    
+    
 }
