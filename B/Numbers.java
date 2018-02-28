5c570d589136d48786ca657ddb54d54eb471929a
KateGJ <36626226+KateGJ@users.noreply.github.com>
2018-02-28 08:59:37 +0100

    Update Numbers.java

--- B/Numbers.java
+++ B/Numbers.java
@@ -1,13 +1,30 @@
-package concurrent_assignment1.B;
+package B;
 
-/**
- * Create class Numbers which instantiates 5 T_PrintID objects (with ID 1 to 5) and runs them.
- * Can you detect some problem? Can you explain the reason?
- * 
- * @author yournamehere
- *
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Numbers {
-
+    
+    public static void main(String[] args) throws InterruptedException{
+        
+        int N=10;
+        
+        Thread h = Thread.currentThread();
+        
+        T_PrintID h1 = new T_PrintID(1, N);
+        T_PrintID h2 = new T_PrintID(2, N);
+        T_PrintID h3 = new T_PrintID(3, N);
+        T_PrintID h4 = new T_PrintID(4, N);
+        T_PrintID h5 = new T_PrintID(5, N);
+        
+        h1.start();
+        h2.start();
+        h3.start();
+        h4.start();
+        h5.start();
+        
+        
+    }
+    
 }
