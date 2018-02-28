fd398b80e959091f42517833dbffb2a62497cb75
KateGJ <36626226+KateGJ@users.noreply.github.com>
2018-02-28 09:00:39 +0100

    Update Numbers.java

--- C/Numbers.java
+++ C/Numbers.java
@@ -1,13 +1,31 @@
-package concurrent_assignment1.C;
+package C;
 
-/**
- * Create class Numbers which instantiates 5 R_PrintID objects (with ID 1 to 5) and runs them.
- * 
- * 
- * @author yournamehere
- *
- */
+import C.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Numbers {
-
+    
+    public static void main(String[] args) throws InterruptedException{
+        
+        int N=10;
+        
+        Thread h = Thread.currentThread();
+       
+        R_PrintID h1 = new R_PrintID(1, N);
+        R_PrintID h2 = new R_PrintID(2, N);
+        R_PrintID h3 = new R_PrintID(3, N);
+        R_PrintID h4 = new R_PrintID(4, N);
+        R_PrintID h5 = new R_PrintID(5, N);
+        
+        h1.start();
+        h2.start();
+        h3.start();
+        h4.start();
+        h5.start();
+        
+        
+    }
+    
 }
