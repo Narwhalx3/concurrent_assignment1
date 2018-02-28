29b26e23336453d346c492af293a0f247b95a4ad
KateGJ <36626226+KateGJ@users.noreply.github.com>
2018-02-28 09:01:49 +0100

    Update Lazy.java

--- D/Lazy.java
+++ D/Lazy.java
@@ -1,17 +1,40 @@
-package concurrent_assignment1.D;
+package D;
 
-/** Lazy must:
- * - Extend Thread
- * - Enter in a loop which iterates a random number of times between 2 to 8.
- * - Sleep 1 second
- * - At each iteration, it displays a random message among: “I am dressing up…”, “Just a sec, please…”, or “These clothes do not suit me…”.
- * - If Hurry interrupts Lazy before it is finished, Lazy claims: “That’s not cricket, please play the game!”.
- * - If Lazy finishes before being interrupted, it says “I am ready, the early bird catches the worm!”.
- * 
- * @author yournamehere
- *
- */
+import java.util.logging.Logger;
 
-public class Lazy {
+public class Lazy implements Runnable {
+    
+    @Override
+    public void run() {
+        
+        Thread Lazy = Thread.currentThread();
+        Lazy.setName("Lazy: ");
+        double random;
+        int r, r2;
+        
+        random = (int) (Math.random()*6+2); //random number between 2 to 8
+        r=(int)random;
+        
+        try {
+            while(r!=0){
+                random = Math.random()*6+2; //para frase
+                r2=(int)random;
+                if(Lazy.isInterrupted()){
+                    System.out.println(Lazy.getName()+"That's no cricket, please play the game!");
+                    return;
+                }
 
+                if(r2<=3) System.out.println(Lazy.getName()+"I am dressing up...");
+                if(r2>3 && r<=6) System.out.println(Lazy.getName()+"Just a sec, please...");
+                if(r2>6) System.out.println(Lazy.getName()+"These clothes do not suit me...");
+                Lazy.sleep(1000);
+                r--;
+                
+                if(r==0) {System.out.println(Lazy.getName()+"I am ready, the early bird catches the worm!"); Hurry.fin=true;  return;}
+            }
+            return;
+        } catch (InterruptedException ex) {
+            
+        }
+    }
 }
